31b7ef4cdb211e2485eea14921b9b10cb516680b
SOG-gen <raheemolalekanusman84@gmail.com>
2024-05-23 15:06:56 +0100

    redirected / to dashboard

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\TeachersController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    redirect('/dashboard');
 });
 
 Route::post('/logout', function () {
